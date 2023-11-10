2fb8ff52457a08ccbdb7b30d52e2b92bca742386
Yuliya <berzhanina.yuliya@gmail.com>
2023-11-10 22:04:32 +0300

    task3

---
+++ FD3/ishop2/App.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Ishop from './components/Ishop';
+
+const nameText='Добро пожаловать в интернет-магазин ToysForYou';
+
+import toysArr from './toys.json';
+
+ReactDOM.render(
+  <Ishop 
+    name={nameText}
+    toys={toysArr}
+  />
+  , document.getElementById('container') 
+);
