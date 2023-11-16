224ac805b92896722acb47a3ab60ef888f51d389
Yuliya <berzhanina.yuliya@gmail.com>
2023-11-16 12:08:48 +0300

    task3

---
+++ ishop2/components/Ishop.js
@@ -0,0 +1,51 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Ishop.css';
+
+import ShopName from './ShopName';
+import Toy from './Toy';
+
+class Ishop extends React.Component {
+
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    toys: PropTypes.array,
+};
+
+  state = {
+    toys: this.props.toys,
+    selectedToyCode: null,
+   }
+
+   selectedToy = toyCode => {
+    this.setState( {selectedToyCode:toyCode})
+   };
+
+   deleteToy = toyCode => {
+    const newToys=this.state.toys.filter(toys => toys.code!==toyCode );
+    this.setState( {toys:newToys})
+   };
+
+  render() {
+    const toysCode=this.state.toys.map( v =>
+      <Toy
+        key={v.code} text={v.text} count={v.count} price={v.price} photo={v.photo} title={v.title}
+        checked={v.checked} code={v.code} 
+       selectedToyCode={this.state.selectedToyCode}
+       cbSelected={this.selectedToy}
+       cbDelete={this.deleteToy} />
+    );
+
+    return (
+      <div className='Ishop'>
+        <ShopName name={this.props.name}/>
+        <div className='Toy'>{toysCode}</div><br></br>
+       
+      </div>
+     
+    );
+  }
+}
+
+export default Ishop;
---
+++ ishop2/components/ShopName.js
@@ -0,0 +1,31 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './ShopName.css';
+
+class ShopName extends React.Component {
+
+    static propTypes = {
+      name: PropTypes.string.isRequired,
+  };
+    
+  render() {
+       
+    return <div className='ShopName'>{this.props.name}<br></br>
+     <table style={{background:"beige"}} >
+       <tbody>
+           <tr >
+             <td className='Text'>{"наименование"}</td>
+             <td className='Count'>{"количество"}</td>
+             <td className='Photo'>{"фото"}</td>
+             <td className='Price'>{"цена"}</td>
+             <td></td>
+             </tr>   
+           </tbody>    
+         </table>
+    </div>;
+  }
+
+}
+
+export default ShopName;
