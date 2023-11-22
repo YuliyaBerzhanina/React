9f66717c0027a9736e818a919c0c361e084da0d2
Yuliya <berzhanina.yuliya@gmail.com>
2023-11-22 15:29:40 +0300

    task4

---
+++ rainbowFrame/App.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RainbowFrame from './components/rainbowFrame';
+
+let colorsArr = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple']; 
+
+ReactDOM.render(
+  <RainbowFrame colors={colorsArr}>
+      Hello!
+    </RainbowFrame>
+  , document.getElementById('container') 
+);
---
+++ rainbowFrame/components/rainbowFrame.js
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './rainbowFrame.css';
+
+
+class RainbowFrame extends React.Component {
+
+  static propTypes = {
+    colors: PropTypes.array,
+};
+
+  render() {
+    let code=this.props.children;
+      this.props.colors.forEach( color => {
+        code= <div className='RainbowFrame' style={{border: "solid 3px "+color}}>{code}</div>
+      }) 
+    return (code);
+  }
+}
+
+export default RainbowFrame;
