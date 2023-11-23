10b2102c070f2c55a9674918b9cee0f10e4f2b8a
Yuliya <berzhanina.yuliya@gmail.com>
2023-11-23 16:23:51 +0300

    task5

---
+++ br2jsx/components/br2jsx.js
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './br2jsx.css';
+
+class Br2jsx extends React.Component {
+
+  static propTypes = {
+    text: PropTypes.string,
+};
+
+  render() {
+   let words=this.props.text.split(/<br *\/?>/);
+   let jsxArr=[];
+   for ( let i=0; i<words.length; i++) {
+    jsxArr.push(words[i]);
+    if (i<words.length-1) {
+      jsxArr.push(<br/>);
+    }
+   }
+
+    return <div className='br2jsx'>{jsxArr}</div>;
+  }
+}
+
+export default Br2jsx;
